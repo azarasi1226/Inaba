c39dde4aef0641bf18aa5c054969eb20354b1298
和樹 安藤 <seal1226.jp@gmail.com>
2023-03-23 23:39:57 +0900

    add:client側にGoogleOIDC追加。多分使わなくなるけどな！！！ｗｗｗ

--- Presentation/Inaba.Presentation.Client/Program.cs
+++ Presentation/Inaba.Presentation.Client/Program.cs
@@ -8,7 +8,10 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+// HTTP
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+// GRPC
 builder.Services.AddSingleton(services =>
 {
     var url = (builder.Configuration["serverUrl"]) ?? throw new NullReferenceException("Server‚ÌUrl‚ªÝ’è‚³‚ê‚Ä‚¢‚Ü‚¹‚ñ");
@@ -16,7 +19,12 @@ builder.Services.AddSingleton(services =>
     var httpHandler = new GrpcWebHandler(GrpcWebMode.GrpcWeb, new HttpClientHandler());
     return GrpcChannel.ForAddress(url, new GrpcChannelOptions { HttpHandler = httpHandler });
 });
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// GoogleOIDC
+builder.Services.AddOidcAuthentication(options =>
+{
+    builder.Configuration.Bind("GoogleOIDC", options.ProviderOptions);
+});
 
 
 await builder.Build().RunAsync();
